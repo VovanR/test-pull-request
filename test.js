5792d2351822bf1f4f6f4e47d31ca08177910fbd
dorofeev <dorofeev@actimind.com>
2018-06-26 13:27:02 +0300

    for test code review

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-export default class CookieConsent{
-
-	constructor(){
-
+export default class CookieConsent {
+	constructor() {
 		this.selectorsConfirm = '.cookie-consent__got-it, .cookie-consent__got-it--mobile';
 		this.selectorsConsent = '.cookie-consent, .cookie-consent--mobile';
 		this.cookieConfirmName = 'cookieConsentConfirm';
@@ -10,27 +8,25 @@ export default class CookieConsent{
 		this.confirms = document.querySelectorAll(this.selectorsConfirm);
 	}
 
-	_popupOpen(){
-
-		if(document.cookie.indexOf(this.cookieConfirmName + '=true') !== -1)
+	_popupOpen() {
+		if (document.cookie.indexOf(this.cookieConfirmName + '=true') !== -1)
 			return false;
 
-		for(let i=0;i<this.consents.length;i++){
+		for(let i=0; i<this.consents.length; i++){
 			this.consents[i].classList.remove(this.consentHideClass);
 		}
 
-		for(let i=0;i<this.confirms.length;i++){
+		for (let i=0; i<this.confirms.length; i++){
 			this.confirms[i].addEventListener('click', () => {this._popupClose();});
 		}
 	}
 
-	_popupClose(){
-
+	_popupClose() {
 		const oneYear = 365*24*60*60*1000;
 		let date = new Date(new Date().getTime() + oneYear);
 		document.cookie = `${this.cookieConfirmName}=true; path=/; expires=${date.toUTCString()}`;
 
-		for(let i=0;i<this.consents.length;i++){
+		for (let i=0; i<this.consents.length; i++) {
 			this.consents[i].classList.add(this.consentHideClass);
 		}
 	}
